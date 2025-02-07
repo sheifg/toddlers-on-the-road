ad04caa0e9f2aac054316514fea1de883345581c
sheila <sheila.fernandez.gonzalez@gmail.com>
2025-02-07 08:59:00 +0100

    Update hero section

--- frontend/src/components/HeroSection.tsx
+++ frontend/src/components/HeroSection.tsx
@@ -15,10 +15,10 @@ const HeroSection = ({handleClick}: HeroSectionProps) => {
       <div className="absolute inset-0 bg-black opacity-40"></div>
       <div className="absolute font-Mali">
         <div className="relative h-full flex flex-col items-center justify-center text-white">
-          <h2 className="text-xl md:text-4xl lg:text-6xl xl:text-7xl mb-2 md:mb-8 font-semibold drop-shadow-lg">Let's explore</h2>
-          <h1 className="text-2xl md:text-6xl lg:text-8xl xl:text-9xl mb-2 md:mb-8 font-bold drop-shadow-lg">the World</h1>
-          <h2 className="text-xl md:text-4xl lg:text-6xl xl:text-7xl mb-2 md:mb-8 font-semibold drop-shadow-lg">
-            with Toddlers on the Road
+        <h2 className="text-xl md:text-4xl lg:text-6xl xl:text-7xl mb-2 md:mb-8 font-semibold drop-shadow-lg">Let's explore</h2>
+          <h1 className="text-xl md:text-4xl lg:text-6xl xl:text-7xl mb-2 md:mb-8 font-semibold drop-shadow-lg">the world with</h1>
+          <h2 className="text-2xl md:text-5xl lg:text-7xl xl:text-8xl mb-2 md:mb-8 font-bold drop-shadow-lg">
+            Toddlers on the Road
           </h2>
           <button type="button" onClick={handleClick} className="px-2 py-2 text-sm md:text-xl lg:text-2xl xl:text-3xl lg:mt-4 bg-blue-water rounded-lg font-semibold hover:bg-light-pink hover:text-marine-blue focus:ring-4 focus:ring-marine-blue transition-colors">
             Find out more
