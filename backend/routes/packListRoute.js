4c22a260f13c539014ad1cbdf94d764ffc6959f2
Sheila Fernandez Gonzalez <126667454+sheifg@users.noreply.github.com>
2025-02-10 14:39:36 +0100

    Update packListRoute.js

--- backend/routes/packListRoute.js
+++ backend/routes/packListRoute.js
@@ -1,12 +1,6 @@
 const router = require("express").Router();
 // Import controller:
 const PackList = require("../controllers/packlistController");
-// Authentication middleware :
-const authMw = require("../middlewares/authMiddleware"); /* Protecting the user route: without log in is not possible to access the PackList route */
-
-const { isLogged } = require("../middlewares/permissions");
-
-
 
 router
   .route("/")
@@ -16,8 +10,8 @@ router
 router
   .route("/:id")
   .get(PackList.read)
-  .put(authMw,isLogged,PackList.update)
-  .patch(authMw,isLogged,PackList.update)
-  .delete(authMw,isLogged,PackList.delete);
+  .put(PackList.update)
+  .patch(PackList.update)
+  .delete(PackList.delete);
 
 module.exports = router;
