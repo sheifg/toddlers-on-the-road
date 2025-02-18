562bad73081b599ab5fa69ef608d6b24dd639e04
sheila <sheila.fernandez.gonzalez@gmail.com>
2025-02-18 15:51:17 +0100

    Implement About page

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,34 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      colors: { 
+      colors: {
         "blue-water": "#60D3D6",
         "marine-blue": "#205D76",
-        "mustard": "#FFC751",
-        "beige": "#FAE9CC",
+        mustard: "#FFC751",
+        beige: "#FAE9CC",
         "light-pink": "#FFA4AB",
         "lawn-green": "#20B376",
         "poppy-red": "#E25764",
-        "black": "#252525",
-        "white": "#F9F9F9",
-        "gray": {
+        black: "#252525",
+        white: "#F9F9F9",
+        gray: {
           100: "#E1E1E1",
           200: "#B5B5B5",
           300: "#495057",
         },
-    },
-    fontFamily: {
-      "Mali": ["Mali", "serif"],
-      "Roboto": ["Roboto", "serif"],
-    }
+      },
+      fontFamily: {
+        Mali: ["Mali", "serif"],
+        Roboto: ["Roboto", "serif"],
+      },
     },
   },
   plugins: [],
-}
-
+};
