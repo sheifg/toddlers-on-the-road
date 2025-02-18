c5ebf302418294fa98dc6280a7c2bbd5434c2d71
Sheila Fernandez Gonzalez <126667454+sheifg@users.noreply.github.com>
2025-02-18 15:42:42 +0100

    Update PackListContainer.tsx

--- frontend/src/components/PackListContainer.tsx
+++ frontend/src/components/PackListContainer.tsx
@@ -2,8 +2,12 @@ import { useState, useEffect } from "react";
 import PackListCard from "./PackListCard";
 import { PackListContextProps } from "../context/PackListContext";
 
-const PackListContainer = ({ packLists, handleAdd, handleEdit, handleAddNewList }: PackListContextProps) => {
-    
+const PackListContainer = ({
+  packLists,
+  handleAdd,
+  handleEdit,
+  handleAddNewList,
+}: PackListContextProps) => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [cardsPerView, setCardsPerView] = useState(1);
 
@@ -19,21 +23,19 @@ const PackListContainer = ({ packLists, handleAdd, handleEdit, handleAddNewList 
       }
     };
 
-    updateCardsPerView(); 
+    updateCardsPerView();
     window.addEventListener("resize", updateCardsPerView);
     return () => window.removeEventListener("resize", updateCardsPerView);
   }, []);
 
   // Navigation for carousel
   const handleNext = () => {
-
     if (currentIndex + cardsPerView < packLists.length) {
       setCurrentIndex((prevIndex) => prevIndex + 1);
     }
   };
 
   const handlePrev = () => {
-  
     if (currentIndex > 0) {
       setCurrentIndex((prevIndex) => prevIndex - 1);
     }
@@ -41,7 +43,7 @@ const PackListContainer = ({ packLists, handleAdd, handleEdit, handleAddNewList 
 
   return (
     <div className="flex flex-col items-center">
-      <h2 className="font-Mali text-center mt-10 text-xl md:text-2xl lg:text-3xl   2xl:text-4xl font-bold text-marine-blue">
+      <h2 className="font-Mali text-center mt-10 text-xl md:text-2xl lg:text-3xl 2xl:text-4xl font-bold text-marine-blue">
         What do I need?
       </h2>
 
@@ -49,39 +51,38 @@ const PackListContainer = ({ packLists, handleAdd, handleEdit, handleAddNewList 
       <div className="relative w-11/12 mt-6">
         <div className="flex justify-between items-center gap-4">
           {/* Left Button */}
-         {currentIndex > 0 && ( 
-          <button 
-            className="w-8 h-8 md:w-10 md:h-10 text-white bg-blue-water rounded-full flex items-center justify-center transition-colors"
-            onClick={handlePrev}
-          >
-            &lt;
-          </button>
-             )} 
+          {currentIndex > 0 && (
+            <button
+              className="w-8 h-8 md:w-10 md:h-10 text-white bg-blue-water rounded-full flex items-center justify-center transition-colors"
+              onClick={handlePrev}
+            >
+              &lt;
+            </button>
+          )}
 
           {/* Cards Display */}
-          <div className=" mx-auto grid  grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-full  lg:text-lg 2xl:gap-10">
-            {packLists.slice(currentIndex, currentIndex + cardsPerView).map((packList, index) => (
-              <div 
-                key={index} 
-                className="  rounded-lg  lg:text-lg"
-              >
-                <PackListCard 
-                  packList={packList}
-                  handleAdd={handleAdd}
-                  handleEdit={handleEdit}
-                />
-              </div>
-            ))}
+          <div className="mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 w-full lg:text-lg 2xl:gap-10">
+            {packLists
+              .slice(currentIndex, currentIndex + cardsPerView)
+              .map((packList, index) => (
+                <div key={index} className="rounded-lg lg:text-lg">
+                  <PackListCard
+                    packList={packList}
+                    handleAdd={handleAdd}
+                    handleEdit={handleEdit}
+                  />
+                </div>
+              ))}
           </div>
 
           {/* Right Button */}
           {currentIndex + cardsPerView < packLists.length && (
-          <button 
-            className="w-8 h-8 md:w-10 md:h-10 text-white bg-blue-water rounded-full flex items-center justify-center transition-colors"
-            onClick={handleNext}
-          >
-            &gt;
-          </button>
+            <button
+              className="w-8 h-8 md:w-10 md:h-10 text-white bg-blue-water rounded-full flex items-center justify-center transition-colors"
+              onClick={handleNext}
+            >
+              &gt;
+            </button>
           )}
         </div>
       </div>
