5d994c5c25f3373219a9de94623ac359bba5861d
sheila <sheila.fernandez.gonzalez@gmail.com>
2025-02-27 21:33:23 +0100

    Refactor packlists

--- backend/controllers/profileController.js
+++ backend/controllers/profileController.js
@@ -1,55 +1,28 @@
 "use strict";
-const Profile= require("../models/profileModel");
+const User = require("../models/userModel");
 
 module.exports = {
-
-  list: async (req, res) => {
-    
-      const data = await res.getModelList(Profile);
-      
-      res.status(200).send({
-        error: false,
-        details: await res.getModelListDetails(Profile),
-        data,
-      
-      });
+  read: async (req, res) => {
+    const data = await User.findOne(
+      { _id: req.params.id },
+      { packLists: 1 }
+    );
+    res.status(200).send({
+      error: false,
+      data,
+    });
   },
 
-  create: async (req, res) => {
-       const data = await Profile.create(req.body);
-       res.status(201).send({
-         error: false,
-         data,
-       });
-     },
-
-  read: async (req, res) => {
-         const data = await Profile.findOne({ _id: req.params.id });
-         res.status(200).send({
-           error: false,
-           data,
-         });
-       },
-
   update: async (req, res) => {
-      const data = await Profile.findByIdAndUpdate(
-        { _id: req.params.id },
-        req.body,
-        { new: true, runValidators: true }
-      );
-      res.status(202).send({
-        error: false,
-        data,
-      });
-    },
-
-   delete: async (req, res) => {
-       const data = await Profile.deleteOne({ _id: req.params.id });
-   
-       res.status(data.deletedCount ? 204 : 404).send({
-         error: !data.deletedCount,
-         data,
-       });
-     }, 
-     
-}
+    console.log('body', req.body)
+    const data = await User.findByIdAndUpdate(
+      { _id: req.params.id },
+      req.body,
+      { new: true, runValidators: true }
+    );
+    res.status(202).send({
+      error: false,
+      data,
+    });
+  }
+};
--- backend/routes/profileRoute.js
+++ backend/routes/profileRoute.js
@@ -2,18 +2,11 @@ const router = require("express").Router();
 
 const Profile = require("../controllers/profileController");
 // Authentication middleware :
-const authMw = require("../middlewares/authMiddleware"); 
-
-router
-  .route("/")
-  .get(Profile.list)
-  .post(authMw ,Profile.create);
+const authMw = require("../middlewares/authMiddleware");
 
 router
   .route("/:id")
-  .get(authMw ,Profile.read)
-  .put(authMw ,Profile.update)
-  .patch(authMw ,Profile.update)
-  .delete(authMw ,Profile.delete);
+  .get(authMw, Profile.read)
+  .put(authMw, Profile.update)
 
 module.exports = router;
