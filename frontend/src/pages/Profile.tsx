04f0260adabc6803f4de96d9ad7c1b60e2594ae5
sara <saraalhafiy89@gmail.com>
2025-03-06 22:25:04 +0100

    implement the functionality of the packLisContainer

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,9 +1,27 @@
+import { useEffect } from "react";
 import { useAuth } from "../context/AuthContext";
+import {
+  ProfileContextProps,
+  useProfileContext,
+} from "../context/ProfileContext";
+import ProfilePackListContainer from "../components/ProfilePackListContainer";
 
 const Profile = () => {
-  const {userInfo} = useAuth();
-  console.log("User:", userInfo)
-  return <div>Profile</div>;
+  const { userInfo } = useAuth();
+  console.log("User:", userInfo);
+  const { loadProfile } = useProfileContext() as ProfileContextProps;
+
+  useEffect(() => {
+    if (userInfo) {
+      loadProfile();
+    }
+  }, []);
+
+  return (
+    <>
+      <ProfilePackListContainer />
+    </>
+  );
 };
 
 export default Profile;
