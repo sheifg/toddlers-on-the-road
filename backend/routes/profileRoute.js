b09b8915e082f53259b761a52201f3f9c1cdc8f7
sheila <sheila.fernandez.gonzalez@gmail.com>
2025-03-19 10:09:18 +0100

    Update milestones functionalities

--- backend/routes/profileRoute.js
+++ backend/routes/profileRoute.js
@@ -9,8 +9,6 @@ const upload = require('../middlewares/upload')
 router
   .route("/:id")
   .get(authMw, Profile.read)
-  // .put(authMw, Profile.update)
-  // .put(authMw, upload.array('images'), Profile.update)
   .put(authMw, Profile.update)
 
 module.exports = router;
